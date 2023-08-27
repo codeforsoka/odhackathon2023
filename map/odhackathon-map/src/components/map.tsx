4ebb49aab8eea941dfed7e3558fda078565e92d9
Masakazu Kawazu <>
2023-08-27 11:04:38 +0900

    通知の仕組みをReactに実装

--- map/odhackathon-map/src/components/map.tsx
+++ map/odhackathon-map/src/components/map.tsx
@@ -1,11 +1,12 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import GPS from './gps'
 import BabyMaker from './babyMaker'
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import babyImage from '../image/baby.png';
 
 type Props = {
   lat: number
@@ -22,6 +23,34 @@ const Map: FC<Props> = (props) => {
       className: `default-marker ${color}`,
     });
   };
+  const notify = () => {
+    // @ts-ignore
+    Push.create("こんにちは!!", {
+      body: "あざらしです!!",
+      icon: babyImage,
+      tag: "myTag",
+      timeout: 12000,
+      vibrate: [100, 100, 100],
+      onClick: function (e: any) {
+          console.log("onClick", e);
+      },
+      onShow: function (e: any) {
+          console.log("onShow", e);
+      },
+      onClose: function (e: any) {
+          console.log("onClose", e);
+      },
+      onError: function (e: any) {
+          console.log("onError", e);
+      }
+    });
+  }
+  useEffect(() => {
+    const head = document.getElementsByTagName('head')[0] as HTMLElement;
+    const scriptUrl = document.createElement('script');
+    scriptUrl.src = 'https://cdnjs.cloudflare.com/ajax/libs/push.js/1.0.12/push.min.js';
+    head.appendChild(scriptUrl);
+  }, []);
   return (
     <MapContainer
       center={[lat, lng]}
@@ -35,7 +64,7 @@ const Map: FC<Props> = (props) => {
       />
       <Marker position={[lat, lng]} icon={colorMarker("red")}>
         <Popup>
-          現在の位置です
+          <input type="button" id="push" onClick={() => notify()} value="クリックするとプッシュ通知が送られます" />
         </Popup>
       </Marker>
       <BabyMaker />
